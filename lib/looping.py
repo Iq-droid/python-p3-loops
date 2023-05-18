bb695648b56f0c4b4214eb786df5819ffd6c1d0a
iqra-ali <iqra.ali@student.moringaschool.com>
2023-05-19 00:34:40 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    squared_list = [num ** 2 for num in int_list if isinstance(num, int)]
+    return squared_list
     pass
 
 def fizzbuzz():
     # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
